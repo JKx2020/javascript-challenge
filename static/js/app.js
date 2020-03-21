7bd385d8b7c4f7091aa8ee278ed1e396a0a973e6
Jackie <kamprat2@gmail.com>
2020-03-21 15:52:48 -0400

    html table

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,21 @@
+////CREATE TABLE
+
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// create a variable to reference the table body from the html
+var tbody = d3.select("tbody");
+
+//loop through the data and append tr & td to the tbody with the data object data
+data.forEach(sighting => {
+    //set row variable and append the table row before you loop through the objects data so that all the data will be 'inline' instead of show up in a column
+    var row = tbody.append("tr");
+
+    Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+
+    });
+})
+
+
