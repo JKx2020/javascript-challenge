3a617617989f32b9bf3477a43c6350b4ff369658
Jackie <kamprat2@gmail.com>
2020-03-21 16:21:02 -0400

    adding filter

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,37 @@
 ////CREATE TABLE
 
-// from data.js
+// pulls in data from data.js
 var tableData = data;
 
 // create a variable to reference the table body from the html
 var tbody = d3.select("tbody");
 
-//loop through the data and append tr & td to the tbody with the data object data
+//loop through the data file objects and append tr & td to the tbody with the data object data
 data.forEach(sighting => {
-    //set row variable and append the table row before you loop through the objects data so that all the data will be 'inline' instead of show up in a column
+    //set row variable and append the table row before you loop through the objects data 
     var row = tbody.append("tr");
-
+    //loop through object data and append the table
     Object.entries(sighting).forEach(([key, value]) => {
         var cell = row.append("td");
         cell.text(value);
-
     });
 })
+
+
+////EDIT FILTER SO IT TAKES USER INPUT AND FILTERS 
+
+//Select the Filter Table button
+var button = d3.select("button");
+
+//Add listener
+button.on("click", function() {
+    // Select the input element where the user will add in the filter date
+    //get the value property of the input element
+    var input = d3.select("#datetime").property("value");
+    console.log(input);
+
+    //set filter to the input value
+    var filteredData = data.filter(dateInput => data.Date === input);
+});
 
 
