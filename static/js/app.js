68892ad142a24187a5a987071e31fd5be9ede04d
Jackie <kamprat2@gmail.com>
2020-03-21 18:04:12 -0400

    table updated for filterd data

--- static/js/app.js
+++ static/js/app.js
@@ -19,29 +19,35 @@ tableData.forEach(sighting => {
 })
 
 
-////EDIT FILTER SO IT TAKES USER INPUT AND FILTERS 
+////UPDATE FILTER SO IT TAKES USER INPUT AND FILTERS 
 
 //Select the Filter Table button
 var button = d3.select("button");
 
-//Add listener to button and record the user input
+//Add listener to button and record the user input & then react accordingly
 button.on("click", function() {
 
     //get the user input date
     var userInputDate = d3.select("#datetime").property("value");
     console.log("The user input date is:", userInputDate);
 
-    //filter by the input value
+    //filter by the input date
     var filteredData = tableData.filter(sighting => sighting.datetime === userInputDate);
 
     console.log(filteredData);
-    
-    //see filtered data
-    filteredData.forEach(data => {
-        Object.entries(filteredData).forEach(([key, value]) => {
-            console.log( value);
+
+    //clear old table & update table for the filtered data
+    d3.selectAll("td").remove();
+
+    filteredData.forEach(sightings => {
+        var row = tbody.append("tr");
+        Object.entries(sightings).forEach(([key, value]) => {
+            var cell = tbody.append("td");
+            cell.text(value);
         });
-    });
+    }); 
+    
+
 });
 
 
